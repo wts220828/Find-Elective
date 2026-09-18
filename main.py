3c06b121be2a759574bec48712434bd7a5e77a00
wts220828 <150510706+wts220828@users.noreply.github.com>
2026-09-18 01:20:48 +0000

    LOREMIPSUM

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import csv
-
-
+file_path = "./F4.csv"
 
 totalElective = ["PHY"
 ,"CHEM"
@@ -14,34 +13,6 @@ totalElective = ["PHY"
 ,"HIST"
 ,"VA"] 
 
-
-
-def gen(file):
-    print(file + ":")
-
-    stuElective = []
-    combElective = []
-    with open(file, mode='r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            stuElective.append(row)
-
-
-    for i in range(0, len(totalElective)):
-        for j in range(i+1, len(totalElective)):
-            combElective.append((totalElective[i], totalElective[j]))
-
-    for i in range(len(stuElective)):
-        for j in range(len(combElective)):
-            if combElective[j][0] in stuElective[i] and combElective[j][1] in stuElective[i]:
-                combElective[j] = "NA"
-
-
-    for i in range(len(combElective)):
-        if combElective[i] != "NA":
-            print(combElective[i])
-            
-
-gen("F4.csv")
-gen("F5.csv")
-gen("F6.csv")
+with open(file_path, mode='r', encoding='utf-8') as csv_file:
+    reader = csv.reader(csv_file)
+    pass
