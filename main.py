1d7a00f6e800a7241a9e437eb33745b3189bf1c8
wts220828 <150510706+wts220828@users.noreply.github.com>
2026-09-18 01:40:44 +0000

    still under construct

--- main.py
+++ main.py
@@ -12,7 +12,25 @@ totalElective = ["PHY"
 ,"CLIT"
 ,"HIST"
 ,"VA"] 
-
+ds1 = []
 with open(file_path, mode='r', encoding='utf-8') as csv_file:
-    reader = csv.reader(csv_file)
-    pass
+    reader1 = csv.reader(csv_file)
+    for row in reader1:
+        ds1.append(row)
+dus=[]
+for ia in totalElective:
+    for ib in totalElective:
+        if ia != ib and not [ib, ia] in dus:
+            dus.append( [ia, ib])
+
+#print(dus)
+
+for ic in ds1:
+    for ie in range(3):
+        print((ie+1)%3)
+        print(ic)
+        if [ic[ie], ic[(ie+1)%3]] in dus:
+            dus.remove[ic[ie], ic[(ie+1)%3]]
+            pass
+print(dus)
+    
